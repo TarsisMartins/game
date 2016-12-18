7e395209040d06ead1d1265bc3611928b149817e
Tarsis Martins <tarsispmartins@gmail.com>
2016-12-18 17:51:48 -0500

    Add files via upload

---
+++ Game/Game.java
@@ -0,0 +1,15 @@
+/*
+* Written in memory of the owner of shoujoai.com,
+* who helped me realize I was gay.
+* Rest in Peace, stranger.
+*/
+
+public class Game{
+
+	public static void main(String[] args){
+
+		System.out.println("I'm here! I'm Queer! I want a beer!");
+
+	}//end main
+
+}//end class
