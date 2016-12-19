5b23b7ba86c0f2b5f18b26e00e9d6da13ef8184a
Tarsis Martins <tarsispmartins@gmail.com>
2016-12-19 14:59:10 -0500

    Create Game.java
    took me the better part of a day but finally we have input/output!

--- Game/Game.java
+++ Game/Game.java
@@ -4,11 +4,34 @@
 * Rest in Peace, stranger.
 */
 
+import java.io.*;
+
 public class Game{
 
 	public static void main(String[] args){
 
-		System.out.println("I'm here! I'm Queer! I want a beer!");
+		System.out.println("How old are ya?");
+
+		Reader r = new BufferedReader(new InputStreamReader(System.in));
+   		StreamTokenizer st = new StreamTokenizer(r);
+
+
+		try{
+		while(st.nextToken() != StreamTokenizer.TT_EOF){
+
+    			if(st.ttype == StreamTokenizer.TT_WORD) {
+				if(st.sval.equals("quit")){break;}
+        			System.out.println(st.sval);
+    			} else if(st.ttype == StreamTokenizer.TT_NUMBER) {
+        			System.out.println(st.nval);
+    			} else if(st.ttype == StreamTokenizer.TT_EOL) {
+        			System.out.println();
+    			}
+
+		}//end while
+		}catch(IOException e){System.out.println("You fucked up, kid.");}
+
+		//st.close();
 
 	}//end main
 
